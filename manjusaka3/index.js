fdb207e63b7aa359f9dd8902be4fa4ac975a396d
NeoAtlantis <contact@chaobai.li>
2020-05-09 23:59:54 +0200

    cli command for compiling

---
+++ manjusaka3/index.js
@@ -0,0 +1,19 @@
+const compiler = require("./compiler");
+const runtime = require("./runtime");
+
+function dieWithUsage(){
+    console.log("Usage: node manjusaka2 <compile|run> <input file>");
+    process.exit(1);
+}
+
+const operator = process.argv[2];
+if(!operator) dieWithUsage();
+
+if(operator.slice(0, 1) == "c"){
+    compiler(
+        require("fs").readFileSync(process.argv[3]).toString()
+    ).then(console.log).then(()=>process.exit(0));
+} else if (operator.slice(0, 1) == "r") {
+} else {
+    dieWithUsage();    
+}
